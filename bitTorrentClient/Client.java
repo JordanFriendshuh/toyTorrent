2215628ce338d10ed6d09d626e4932288f4bddaa
Jordan Friendshuh <jordan.friendshuh@gmail.com>
2014-04-21 12:01:21 -0500

    Changed some shit

--- bitTorrentClient/Client.java
+++ bitTorrentClient/Client.java
@@ -11,17 +11,20 @@ public class Client {
 	 */
 	private static ClientPeer myPeer;
 	private static List<Peer> otherPeers;
-	private static int data[];
+	private static List<Integer> data;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		data = new int[10000];
+		data = Collections.synchronizedList(new ArrayList<Integer>(10000));
+		for(int i=0; i<data.size(); i++){
+			data.set(i, -1);
+		}
 		otherPeers = Collections.synchronizedList(new ArrayList<Peer>());
 		serverTalk servCon = new serverTalk(args[1], Integer.parseInt(args[2]), otherPeers, myPeer);
 		servCon.start();
 		
 	}
 	
-	public void clientTalk(){
+	public void clientTalk(int count){
 		myPeer.setTop4Peers(otherPeers);
 		List<Peer> myPeers = myPeer.getTop4Peers();
 		
