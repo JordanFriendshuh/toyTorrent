5391cc6ddac0b8ea6d283f0dd253de8fe13d9bab
Jordan Friendshuh <jordan.friendshuh@gmail.com>
2014-04-21 11:20:34 -0500

    Started on client to client talk

--- bitTorrentClient/Client.java
+++ bitTorrentClient/Client.java
@@ -1,3 +1,4 @@
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,10 +11,20 @@ public class Client {
 	 */
 	private static ClientPeer myPeer;
 	private static List<Peer> otherPeers;
+	private static int data[];
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		data = new int[10000];
 		otherPeers = Collections.synchronizedList(new ArrayList<Peer>());
+		serverTalk servCon = new serverTalk(args[1], Integer.parseInt(args[2]), otherPeers, myPeer);
+		servCon.start();
+		
 	}
 	
+	public void clientTalk(){
+		myPeer.setTop4Peers(otherPeers);
+		List<Peer> myPeers = myPeer.getTop4Peers();
+		
+	}
 	
 }
