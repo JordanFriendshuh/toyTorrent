c4e7e17bf203b3bb594623c695eb3c398cf914b4
Jordan Friendshuh <jordan.friendshuh@gmail.com>
2014-04-21 12:29:33 -0500

    Starting to get there

--- bitTorrentClient/Client.java
+++ bitTorrentClient/Client.java
@@ -2,6 +2,8 @@ import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Client {
@@ -12,8 +14,11 @@ public class Client {
 	private static ClientPeer myPeer;
 	private static List<Peer> otherPeers;
 	private static List<Integer> data;
+	private static List<ClinetRecieve> recivers;
+	private static int count;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		count = 0;
 		data = Collections.synchronizedList(new ArrayList<Integer>(10000));
 		for(int i=0; i<data.size(); i++){
 			data.set(i, -1);
@@ -21,12 +26,23 @@ public class Client {
 		otherPeers = Collections.synchronizedList(new ArrayList<Peer>());
 		serverTalk servCon = new serverTalk(args[1], Integer.parseInt(args[2]), otherPeers, myPeer);
 		servCon.start();
+		recivers.add(new ClinetRecieve(Integer.parseInt(args[2]), myPeer));
+		recivers.add(new ClinetRecieve(Integer.parseInt(args[2]), myPeer));
+		Timer resend = new Timer();
+		resend.schedule(new TimerTask(){
+			public void run(){
+				clientTalk();
+			}
+		}, 1000*10, 1000*10);
 		
 	}
 	
-	public void clientTalk(int count){
+	public static void clientTalk(){
 		myPeer.setTop4Peers(otherPeers);
 		List<Peer> myPeers = myPeer.getTop4Peers();
+		for(ClinetRecieve n : recivers){
+			n.reset();
+		}
 		
 	}
 	
--- bitTorrentClient/ClinetRecieve.java
+++ bitTorrentClient/ClinetRecieve.java
@@ -22,7 +22,7 @@ public class ClinetRecieve extends Thread{
 		this.port = port;
 		this.mainPeer = mainPeer;
 	}
-	public void reset(InetAddress IPin){
+	public void reset(){
 		finish = true;
 	}
 	public void run(){
@@ -34,20 +34,35 @@ public class ClinetRecieve extends Thread{
 				DataOutputStream out = new DataOutputStream(connection.getOutputStream());
 				BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 				ArrayList<Integer> indexes = mainPeer.getFileIndicesAlreadyDLed();
+				int count = in.read();
+				for(int i = 0; i < count; i++){
+					requested.add(in.read());
+				}
 				out.writeByte(indexes.size());
 				for(int i = 0; i < indexes.size(); i++){
 					out.writeByte(indexes.get(i));
 				}
+				requested.retainAll(mainPeer.getFileIndicesAlreadyDLed());
 				while(!finish){
-					
-					
+					int index = in.read();
+					int value = in.read();
+					if(index != -1){
+						data.add(index, value);
+					}
+					if(requested.size() > 0){
+						index = requested.get(0);
+						requested.remove(0);
+						out.writeByte(index);
+						out.writeByte(data.get(index));
+					}else{
+						out.writeByte(-1);
+						out.writeByte(-1);
+					}			
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			
 		}	
 	}
 }
